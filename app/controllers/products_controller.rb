8f41bd017e5e9853d283ae932a44575e2d8ae4c3
JackMarx <seriousfools@gmail.com>
2018-01-18 18:56:21 -0600

    index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,3 @@
 class ProductsController < ApplicationController
-  def all_products_method
-    products = Product.all
-    render json: products.as_json
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get '/all_products_url' => 'products#all_products_method'
+  
 end
