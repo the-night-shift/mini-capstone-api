50e7bb575dd92f68c3617d7134a9a7a3813f84d4
Brian <riceb53@gmail.com>
2018-03-13 11:50:20 -0500

    add as_json and modify db name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,28 @@
 class Product < ApplicationRecord
+  def as_json
+    {
+      id: id,
+      name: name,
+      image_url: image_url,
+      price: price,
+      description: description,
+      is_discounted: is_discounted?,
+      tax: tax,
+      total: total
+    }
+  end
+
+  def is_discounted?
+    price < 20
+  end
+
+  def tax
+    (price * 0.09).round(2)
+  end
+
+  def total
+    price + tax
+  end
 end
+
+
