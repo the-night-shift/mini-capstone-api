05a78f23ac147fea8fde3260934a657c7f8919ce
Brian <riceb53@gmail.com>
2018-03-15 21:24:22 -0500

    use has_many and belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+  has_many :images
+
   validates :name, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
@@ -31,14 +34,6 @@ class Product < ApplicationRecord
 
   def total
     price + tax
-  end
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: id)
   end
 end
 
